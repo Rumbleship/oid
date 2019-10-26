7d0ace388912f660c8fb30c919a41c5d424d7555
Isaac Sparling <223606+isparling@users.noreply.github.com>
2019-10-26 12:19:53 -0700

    :goberserk: new build artifacts

---
+++ lib/src/plain/modern.factory.d.ts
@@ -0,0 +1,11 @@
+import Hashids from 'hashids';
+import { Oid2 } from '../oid';
+import { OidFactory } from '../implementations/oid-factory.interface';
+export declare class ModernOidFactory implements OidFactory {
+    create(scopename: string, id: string | number): Oid2;
+    unwrap(oid: Oid2): {
+        scope: string;
+        id: string | number;
+    };
+    getEncoder(_scopename: string): Hashids;
+}
