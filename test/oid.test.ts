76246ccf2da610fa32d2985d8276f3e164159c51
Isaac Sparling <223606+isparling@users.noreply.github.com>
2019-10-26 12:43:55 -0700

    :shell: add explicit test for alpha hashids

--- test/oid.test.ts
+++ test/oid.test.ts
@@ -88,28 +88,35 @@ describe('Scenario: Experimental Oids can be registered', () => {
   });
 });
 
-describe('Scenario: creating Hashid Oids', () => {
-  describe('Given: a PurchaseOrder scope has been registered', () => {
-    beforeAll(() => {
-      Oid2.RegisterScope('PurchaseOrder', 'po');
-    });
-    describe('When: creating an oid from a known database_id', () => {
-      let oid: Oid2;
-      const database_id = 1;
-      const hashed = 'po_781nx';
+describe('Scenario: creating AlphaHashids', () => {
+  describe.each([
+    ['PurchaseOrder', 'po', 1, 'po_781nx'],
+    ['Shipment', 'shp', 1, 'shp_jdxri'],
+    ['Buyer', 'b', 1, 'b_ngdn2'],
+    ['Supplier', 's', 2, 's_2yon0'],
+    ['User', 'u', 1, 'u_76ykw']
+  ])(
+    'Given:  a %s scope has been registered with shortcode %s',
+    (scopeName, shortcode, database_id, knownHashidFromAlpha) => {
       beforeAll(() => {
-        oid = Oid2.create('PurchaseOrder', database_id);
+        Oid2.RegisterScope(scopeName as string, shortcode as string);
       });
-      test('Then: it hashes mas expected', () => {
-        expect(oid.oid).toBe(hashed);
+      describe('When: creating an oid from a known database_id', () => {
+        let oid: Oid2;
+        beforeAll(() => {
+          oid = Oid2.create(scopeName as string, database_id);
+        });
+        test('Then: it hashes mas expected', () => {
+          expect(oid.oid).toBe(knownHashidFromAlpha);
+        });
+        test('Then: it can be unwrapped', () => {
+          const { scope, id } = oid.unwrap();
+          expect(scope).toBe(scope);
+          expect(id).toBe(database_id);
+        });
       });
-      test('Then: it can be unwrapped', () => {
-        const { scope, id } = oid.unwrap();
-        expect(scope).toBe('PurchaseOrder');
-        expect(id).toBe(database_id);
-      });
-    });
-  });
+    }
+  );
 });
 
 describe('Scenario: creating Modern Oids', () => {
