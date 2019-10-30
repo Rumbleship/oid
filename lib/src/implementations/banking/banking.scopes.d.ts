f079d09d2a066156b03921d41592d720d7f14138
Isaac Sparling <223606+isparling@users.noreply.github.com>
2019-10-30 11:59:11 -0700

    :chart_with_upwards_trend: build artifacts for batch and plaiditem scopes

--- lib/src/implementations/banking/banking.scopes.d.ts
+++ lib/src/implementations/banking/banking.scopes.d.ts
@@ -2,6 +2,7 @@ export declare enum BankingScopeNames {
     AchGateway = "AchGateway",
     BankAccount = "BankAccount",
     BankTransaction = "BankTransaction",
+    Batch = "Batch",
     OrderReference = "OrderReference",
     PaymentRequest = "PaymentRequest"
 }
--- lib/src/implementations/scopes.enum.d.ts
+++ lib/src/implementations/scopes.enum.d.ts
@@ -1,5 +1,6 @@
 export * from './checkdigit/historical.scopes';
 export * from './banking/banking.scopes';
 export declare enum CheckdigitScopes {
-    Company = "Company"
+    Company = "Company",
+    PlaidItem = "PlaidItem"
 }
