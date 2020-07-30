d27ee12106231eb6752f2ba90c831295a52d6d72
Rumbleship Operations Machine User <37818125+rumbleship-ops@users.noreply.github.com>
2020-07-30 16:21:43 +0000

    Release version 4.0.6 [skip ci]

--- lib/implementations/checkdigit/checkdigit.factory.d.ts
+++ lib/implementations/checkdigit/checkdigit.factory.d.ts
@@ -1,3 +1,4 @@
+import Hashids from 'hashids';
 import { OidFactory } from '../oid-factory.interface';
 import { Oid } from '../../oid';
 export declare class CheckdigitOidFactory implements OidFactory {
@@ -16,5 +17,5 @@ export declare class CheckdigitOidFactory implements OidFactory {
         scope: string;
         id: string | number;
     };
-    getEncoder(scopename: string): any;
+    getEncoder(scopename: string): Hashids;
 }
